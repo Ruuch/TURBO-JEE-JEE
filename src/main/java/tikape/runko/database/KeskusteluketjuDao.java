f1e4bb4dde6ac729f5ba5f5265b4befa5e598862
Sadelise <sharhio@hotmail.com>
2017-02-21 13:09:21 +0200

    KeskusteluketjuDao - Uusien keskusteluketjujen lisääminen mahdollista

--- src/main/java/tikape/runko/database/KeskusteluketjuDao.java
+++ src/main/java/tikape/runko/database/KeskusteluketjuDao.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import tikape.runko.domain.Keskusteluketju;
@@ -72,23 +74,23 @@ public class KeskusteluketjuDao implements Dao<Keskusteluketju, Integer> {
 
         return keskusteluketjut;
     }
-    
+
     public List<Keskusteluketju> alueenKetjut(Integer aihealueId) throws SQLException {
-        
+
         Connection connection = database.getConnection();
         PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Keskusteluketju WHERE aihealue_id = ?");
         stmt.setObject(1, aihealueId);
 
         ResultSet rs = stmt.executeQuery();
         List<Keskusteluketju> lista = new ArrayList<>();
-        
+
         while (rs.next()) {
             Integer id = rs.getInt("id");
             Integer aihealue_id = rs.getInt("aihealue_id");
             Integer viestienMaara = rs.getInt("viestienMaara");
             String otsikko = rs.getString("otsikko");
             String aikaleima = rs.getString("aikaleima");
-            
+
             lista.add(new Keskusteluketju(id, aihealue_id, viestienMaara, otsikko, aikaleima));
         }
 
@@ -97,12 +99,25 @@ public class KeskusteluketjuDao implements Dao<Keskusteluketju, Integer> {
         connection.close();
 
         return lista;
-        
+
     }
 
     @Override
     public void delete(Integer key) throws SQLException {
         // ei toteutettu
+    }
+
+    public void save(Integer id, Integer aihealue_id, String otsikko) throws SQLException {
+        LocalDateTime timePoint = LocalDateTime.now();
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("INSERT INTO Keskusteluketju (id, aihealue_id, viestienMaara, otsikko, aikaleima) VALUES (?, ?," + "0," + "?,?);");
+        stmt.setObject(1, id);
+        stmt.setObject(2, aihealue_id);
+        stmt.setObject(3, otsikko);
+        stmt.setObject(4, timePoint.toString());
+        stmt.executeUpdate();
+        stmt.close();
+        connection.close();
     }
 
 }
