eeafec87d12bc58f22c1550d23d874d200583994
manuho <holmmanu@gmail.com>
2017-02-27 10:58:21 +0200

    Add files via upload
    generateId, save ja findOne(String) metodien lisääminen.

--- src/main/java/tikape/runko/database/KayttajaDao.java
+++ src/main/java/tikape/runko/database/KayttajaDao.java
@@ -9,7 +9,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import tikape.runko.domain.Kayttaja;
 
@@ -46,6 +48,31 @@ public class KayttajaDao implements Dao<Kayttaja, Integer> {
 
         return k;
     }
+    
+    public Kayttaja findOne(String nimia) throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Kayttaja WHERE nimi = ?");
+        stmt.setObject(1, nimia);
+        
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+        
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+        String ekaViesti = rs.getString("ekaViesti");
+        Integer viestienMaara = rs.getInt("viestienMaara");
+        
+        Kayttaja k = new Kayttaja(id, nimi, ekaViesti, viestienMaara);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+        
+        return k;
+    }
 
     @Override
     public List<Kayttaja> findAll() throws SQLException {
@@ -74,6 +101,47 @@ public class KayttajaDao implements Dao<Kayttaja, Integer> {
     @Override
     public void delete(Integer key) throws SQLException {
         // ei toteutettu
+    }
+    
+    public void save(String nimi) throws SQLException {
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = new Date();
+        String sDate = sdf.format(date);
+        Integer id = generateId();
+        Connection connection = database.getConnection();
+        
+        PreparedStatement stmt = connection.prepareStatement("INSERT INTO Kayttaja (id, nimi, viestienMaara, ekaViesti) "
+                + "VALUES (?, ?," + "0," + "?);");
+        
+        stmt.setObject(1, id);
+        stmt.setObject(2, nimi);
+        stmt.setObject(3, sDate);
+        stmt.executeUpdate();
+        stmt.close();
+        connection.close();
+    }
+    
+    public Integer generateId() throws SQLException {
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Kayttaja ORDER BY id DESC LIMIT 1");
+        ResultSet rs = stmt.executeQuery();
+        if (!rs.next()) {
+            return null;
+        }
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+        Integer viestienMaara = rs.getInt("viestienMaara");
+        String ekaViesti = rs.getString("ekaViesti");
+
+        Kayttaja k = new Kayttaja(id, nimi, ekaViesti, viestienMaara);
+        int newId = k.getId() + 1;
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return newId;
     }
 
 }
